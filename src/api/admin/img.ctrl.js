334c8947de8dff2f5052317b7462e489a7db4997
dvlprsh <dvlprsh103@gmail.com>
2020-05-11 09:18:18 +0900

    Update admin img.ctrl.js
    1. 센터 이미지 저장소 s3로 변경

--- src/api/admin/img.ctrl.js
+++ src/api/admin/img.ctrl.js
@@ -1,62 +1,35 @@
 import express from 'express';
 import multer from 'multer';
 import mongoose from 'mongoose';
-const uuidv4 = require('uuid/v4');
 const uploader = express.Router();
 const path = require('path');
-// User model
-//let User = require('../models/User');
-import Test from '../../models/imgTest';
-const DIR = `./src/public/centers`;
-const fs = require('fs');
 import checkLoggedIn from '../../lib/checkLoggedIn';
+const AWS=require('aws-sdk');
+const multerS3=require('multer-s3');
 
-
-fs.readdir(DIR, (error) => {
-    if (error) {
-        console.log('centers 폴더가 없어 uploads 폴더를 생성합니다.');
-        console.error('centers 폴더가 없어 uploads 폴더를 생성합니다.');
-        fs.mkdirSync(DIR);
-    }
-});
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, DIR);
-    },
-    filename: (req, file, cb) => {
-        const ext = path.extname(file.originalname);
-        const fileNameLowerCase = file.originalname.toLowerCase().split(' ').join('-');
-        const fileName = path.basename(fileNameLowerCase, ext);
-        cb(null, fileName + new Date().valueOf() + path.extname(file.originalname));
-    }
+AWS.config.update({
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    region: 'ap-northeast-2',
 });
 
 const upload = multer({
-    storage: storage,
-    fileFilter: (req, file, cb) => {
-        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-            cb(null, true);
-        } else {
-            cb(null, false);
-            return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
-        }
-    },
+    storage: multerS3({
+        s3: new AWS.S3(),
+        bucket: 'climbers',
+        acl: 'public-read',
+        key(req, file, cb) {
+            cb(null, `centers/${+new Date()}${path.basename(file.originalname)}`);
+        },
+    }),
     limits: { fileSize: 5 * 1024 * 1024 },
 });
 
 uploader.post('/', upload.single('img'), (req, res, next) => {
     console.log("uploader.post('/upload-images' called");
-    //const reqFiles = [];
-    const url = req.protocol + '://' + req.get('host');
-
-    const user = new Test({
-        _id: new mongoose.Types.ObjectId(),
-        imgCollection: req.file.filename
-    });
 
     res.status(201).json({
-        url: `/centers/${req.file.filename}`
+        url: req.file.location
     });
 
 });
--- src/api/users/index.js
+++ src/api/users/index.js
@@ -3,13 +3,12 @@ import * as usersCtrl from './users.ctrl';
 import checkLoggedIn from '../../lib/checkLoggedIn';
 
 const users=express.Router();
-
 users.get('/', usersCtrl.list);
 
-users.use('/:username', usersCtrl.getUserByUsername, user);
 const user=express.Router(); // /api/users/:username
-
 user.get('/', usersCtrl.read);
 //users.use('/:id', usersCtrl.getUserById, user);
 
+
+users.use('/:username', usersCtrl.getUserByUsername, user);
 export default users;
